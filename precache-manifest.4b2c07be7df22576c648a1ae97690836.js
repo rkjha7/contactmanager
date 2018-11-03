e66c15869a5bc1f1887a640f71d6a5da72977551
Rishabh Jha <rishabh.jha@gmail.com>
2018-11-02 17:54:21 -0700

    Updates

---
+++ precache-manifest.4b2c07be7df22576c648a1ae97690836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "2229815f0246d03443e0",
+    "url": "/contactmanager/static/js/main.2229815f.chunk.js"
+  },
+  {
+    "revision": "d5594d800ca6b61e1927",
+    "url": "/contactmanager/static/js/1.d5594d80.chunk.js"
+  },
+  {
+    "revision": "2229815f0246d03443e0",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "d5594d800ca6b61e1927",
+    "url": "/contactmanager/static/css/1.5a8eec96.chunk.css"
+  },
+  {
+    "revision": "3ce94950f190990d3d2bf5f50ae6a502",
+    "url": "/contactmanager/index.html"
+  }
+];
